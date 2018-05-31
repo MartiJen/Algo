9c605ea30ceb40455cd238d9fe701951b6590d6b
Jennifer <jennifer_martin@outlook.fr>
2018-05-31 15:41:27 +0200

    Methode afficher Menu

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        private static int GererMenu(List<string> menu)
+        {
+            foreach (string ligne in menu)
+            {
+                Console.WriteLine(ligne);
+            }
+
+            int choix;
+            choix = int.Parse(Console.ReadLine());
+            return choix;
+        }
+
+
+    }
+}
