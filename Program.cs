7fc35df3aa9bad050e0680a237ddba5fe6af49ed
Jennifer <jennifer_martin@outlook.fr>
2018-05-31 17:17:45 +0200

    Methode menu suite

--- Program.cs
+++ Program.cs
@@ -8,23 +8,64 @@ namespace ConsoleApp4
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            List<MenuItem> menu = new List<MenuItem>();
+            menu.Add(new MenuItem("choix 2", 2));
+            menu.Add(new MenuItem("choix 4", 4));
+            menu.Add(new MenuItem("choix 6", 6));
+            menu.Add(new MenuItem("choix 8", 8));
+            menu.Add(new MenuItem("choix 10", 10));
 
 
+            int menuChoisi = GererMenu(menu);
+
+            Console.WriteLine("Vous avez choisi : " + menuChoisi);
+            Console.ReadLine();
         }
 
-        private static int GererMenu(List<string> menu)
+        private static int GererMenu(List<MenuItem> menu)
         {
-            foreach (string ligne in menu)
+          
+            foreach (MenuItem ligne in menu)
             {
-                Console.WriteLine(ligne);
+                Console.WriteLine($"{ligne.Index}. {ligne.Libelle}");
             }
 
             int choix;
             choix = int.Parse(Console.ReadLine());
-            return choix;
+
+            while (true)
+            {
+                foreach (MenuItem item in menu)
+                {
+                    if (choix == item.Index)
+                    {
+                        return choix;
+                    }
+                }
+                Console.WriteLine("Erreur, saisir un choix");
+                choix = int.Parse(Console.ReadLine());
+
+            }
+            return -1;
+
+            //return singleOrDefault(x=> x.Index===choix) is null :choix: -1
         }
+
+        public class MenuItem
+        {
+            public string Libelle { get; set; }
+            public int Index { get; set; }
+
+            public MenuItem (string libelle, int index)
+            {
+                Libelle = libelle;
+                Index = index;
+            }
+        }
+
 
 
     }
